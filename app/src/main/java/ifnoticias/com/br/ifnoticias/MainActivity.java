54f10e6705fb8408da30cf1b21429a6522e4ca07
pedrogyn <glad.pedro@gmail.com>
2017-02-24 20:30:46 -0300

    PARSER RODANDO 100%... CRIAR LIST VIEW PARA GERAR NOTICIAS DINAMICAMENTE.

--- app/src/main/java/ifnoticias/com/br/ifnoticias/MainActivity.java
+++ app/src/main/java/ifnoticias/com/br/ifnoticias/MainActivity.java
@@ -7,6 +7,7 @@ import org.jsoup.select.Elements;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.Texto1);
+        textView2 = (TextView) findViewById(R.id.Texto2);
 
         new ParsePage().execute();
     }
@@ -50,13 +53,17 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
 
+            Log.d("POST EXECUTE: ", "onPostExecute: rodou");
+
             ArrayList<String> arrayList = new ArrayList<>();
 
             for(Element e : elements){
                 arrayList.add(e.text().toString());
+                Log.d("Element e ", e.text().toString());
             }
 
-           textView.setText(arrayList.get(0));
+           textView.setText(arrayList.get(0).toString());
+           textView2.setText(arrayList.get(1).toString());
 
         }
     }
